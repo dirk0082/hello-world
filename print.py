3f99164f350333b84333feede1701788520caa0d
dirk0082 <dirk0082@umn.edu>
2017-04-23 16:32:45 -0500

    A little basic math
    Finding the area of a circle

--- print.py
+++ print.py
@@ -1,2 +1,11 @@
-print ("Hello World")
-print ("More and More printing")
+##print ("Hello World")
+##print (13 + 7)
+
+##Find the area of a circle
+pi=3.14
+r=5
+
+print ("What is the area of a circle with radius 5?")
+print ("pi*r^2")
+
+print(pi * r ** 2)
